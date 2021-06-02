fa31c8063db02c0056000e9da20c34945b186fb0
пк <barcats@inbox.ru>
2021-06-02 22:31:23 +0300

    сделал тесты на некоторые классы - StateUtils, например

--- app/src/main/java/com/bartex/statesmvvm/model/utils/StateUtils.kt
+++ app/src/main/java/com/bartex/statesmvvm/model/utils/StateUtils.kt
@@ -5,15 +5,15 @@ import com.bartex.statesmvvm.model.entity.state.State
 class StateUtils:IStateUtils {
 
     override fun getStateArea(area: Float?): String {
-      var   textStateArea = ""
+      var   textStateArea = "Площадь территории неизвестна"
         area?. let {
-            textStateArea = if(it!= 0f ){
+            textStateArea = if(it > 0f ){
                 if (it>1000000f){
                     String.format("Площадь: %.1f млн. кв. км.", (it)/1000000)
                 }else if (it in 1000f..1000000f){
                     String.format("Площадь: %.1f тыс. кв. км.", (it)/1000)
                 }else{
-                    String.format("Площадь: %.1f  кв. км.", it)
+                    String.format("Площадь: %.1f кв. км.", it)
                 }
             }else {
                 "Площадь территории неизвестна"
@@ -23,9 +23,9 @@ class StateUtils:IStateUtils {
     }
 
     override fun getStatePopulation(population: Int?): String {
-        var   textStatePopulation = ""
+        var   textStatePopulation = "Население: численность неизвестна"
         population?. let {
-            textStatePopulation = if (it!=0){
+            textStatePopulation = if (it>0){
                 if(it>1000000){
                     String.format("Население: %.1f млн. чел.", (it.toFloat())/1000000)
                 }else if (it in 1000..1000000){
@@ -41,32 +41,38 @@ class StateUtils:IStateUtils {
     }
 
     override fun getStatezoom(state: State?): String {
-        var zoom = 0
-        state?.area?. let{
-            if (it<10f){
-                zoom = 13
-            }else if (it in 10f..1000f){
-                zoom = 11
-            }else if (it in 1000f..30000f){
-                zoom = 9
-            }else if (it in 1000f..100000f) {
-                zoom = 7
-            }else if (it in 100000f..1000000f){
-                zoom = 5
-            }else if (it in 1000000f..5000000f){
-                zoom = 3
-            }else{
-                zoom = 1
-            }
-        }
+       val  zoom = getZoom(state?.area)
         return String.format("geo:%s,%s?z=%s",
             state?.latlng?.get(0).toString(), state?.latlng?.get(1).toString(), zoom.toString())
     }
 
+
+     fun getZoom(area: Float?): Int {
+        var zoom1 = 0
+        area?.let {
+            if (it < 10f) {
+                zoom1 = 13
+            } else if (it in 10f..1000f) {
+                zoom1 = 11
+            } else if (it in 1000f..30000f) {
+                zoom1 = 9
+            } else if (it in 30000f..100000f) {
+                zoom1 = 7
+            } else if (it in 100000f..1000000f) {
+                zoom1 = 5
+            } else if (it in 1000000f..5000000f) {
+                zoom1 = 3
+            } else {
+                zoom1 = 1
+            }
+        }
+        return zoom1
+    }
+
     override fun getStateCapital(capital: String?): String {
-        var capitalState = ""
+        var capitalState = "Название столицы неизвестно"
         capital?. let {
-            capitalState = if (it != ""){
+            capitalState = if (it.isNotBlank()){
                 String.format("Столица:   %S ", it)
             }else{
                 "Название столицы неизвестно"
@@ -76,9 +82,9 @@ class StateUtils:IStateUtils {
     }
 
     override fun getStateRegion(region: String?): String {
-        var regionNew = ""
+        var regionNew = "Название региона неизвестно"
         region?. let {
-            regionNew = if(it!= ""){
+            regionNew = if(it.isNotBlank()){
                 String.format("Регион:   %S ", it)
             }else{
                 "Название региона неизвестно"
